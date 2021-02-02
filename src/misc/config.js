4c762f71290d655390d6ddd433afec4d21b3cde5
Dibendu Kundu <dibendukundu5@gmail.com>
2021-02-02 19:44:57 +0530

    displayed show results and small fetch

---
+++ src/misc/config.js
@@ -0,0 +1,10 @@
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+
+export async function apiGet(queryString) {
+
+  const responce = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
+     r.json()
+     );
+  return responce;
+}
